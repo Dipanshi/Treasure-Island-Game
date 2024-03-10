fc82ebe44f90837baef2e7a51f01acd288ff6ede
Dipanshi <32461008+Dipanshi@users.noreply.github.com>
2024-03-10 13:23:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+left_right=input("where do you want to go left or right?")
+if left_right.lower()=="left":
+  print("ohh you are safe now you reached on a forest full with crocodiles and snakes. Choice you way wisely!!")
+  swim_boat=input("do you want to swim or wait for boat?")
+  if swim_boat.lower()=='wait':
+    print("you are safe now you reached on a island with three roads one with red flower,one with pink flower and one with orange flower")
+    road=input("which road you choice red, pink or orange?")
+    if road.lower()=='pink':
+      print("ohh lion eats you game over")
+    elif road.lower()=='red':
+      print("ohh you are brun on fire game over")
+    else:
+      print("you found the Treasure yeapppp!!!")
+      print("You Win!!!")
+  else: 
+   print("you are eaten by crocodile game over!!")
+else:
+  print("you are eaten by snake game over!!")
